5093f720dcf7be35408c43f8340d6e540eb894f2
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-06-30 22:07:52 +0200

    Clicking on button specifies grid column length, add additional classes

--- src/components/common/difficulty.tsx
+++ src/components/common/difficulty.tsx
@@ -8,32 +8,56 @@ interface DifficultyProps {
   setCharacters: (newCharacter: { id: string; name: string }[]) => void;
   setDifficulty: (newDifficulty: string | null) => void;
   setView: (value: string) => void;
+  setSelectedClass: (value: string) => void;
 }
 export function Difficulty({
   setCharacters,
   setDifficulty,
   setView,
+  setSelectedClass,
 }: DifficultyProps) {
   function selectDifficulty(e: React.MouseEvent<HTMLButtonElement>) {
     const difficultyName = (e.target as HTMLInputElement).textContent ?? '';
-    difficultyName ? setDifficulty(difficultyName) : null;
+    if (difficultyName) {
+      setDifficulty(difficultyName);
 
-    // set the array of characters
-    const allCharacters = getDifficulty(difficultyName, originalCharacters);
-    setCharacters((prevCharacters) => {
-      return [...prevCharacters, ...allCharacters];
-    });
+      // set the array of characters
+      const allCharacters = getDifficulty(difficultyName, originalCharacters);
+      setCharacters((prevCharacters) => {
+        return [...prevCharacters, ...allCharacters];
+      });
 
-    // advance state
-    setView(viewStates.runGame);
+      // get the corresponding class name
+      const className = getDifficulty(
+        difficultyName,
+        originalCharacters,
+        'gridAmount',
+      );
+
+      // advance states
+      setSelectedClass(className);
+      setView(viewStates.runGame);
+    }
   }
 
   return (
-    <div>
-      <button onClick={selectDifficulty}>Novice</button>
-      <button onClick={selectDifficulty}>Apprentice</button>
-      <button onClick={selectDifficulty}>Master</button>
-      <button onClick={selectDifficulty}>Avatar</button>
+    <div className={'difficulty-container'}>
+      <h2 className="choose-difficulty">Choose your Difficulty</h2>
+
+      <div className="difficulty-button-container">
+        <button onClick={selectDifficulty} className="novice">
+          Novice
+        </button>
+        <button onClick={selectDifficulty} className="apprentice">
+          Apprentice
+        </button>
+        <button onClick={selectDifficulty} className="master">
+          Master
+        </button>
+        <button onClick={selectDifficulty} className="avatar">
+          Avatar
+        </button>
+      </div>
     </div>
   );
 }
