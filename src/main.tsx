b6790524a74d14736c46a826edd85042b37b7588
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-06-30 20:32:59 +0200

    Add import CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+import './css/header.css';
 import './css/main.css';
+import './css/footer.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
