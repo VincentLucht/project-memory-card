a706df2c1c76b72a4233a4f658cfbb9cc190c0e5
VincentLucht <127682245+VincentLucht@users.noreply.github.com>
2024-06-30 20:33:39 +0200

    Add getMessage() function, returns corresponding message to difficulty and win/loose

--- src/typescript/states.ts
+++ src/typescript/states.ts
@@ -4,3 +4,33 @@ export const viewStates = {
   gameOver: 'gameOver',
   wonGame: 'wonGame',
 };
+
+export function getMessage(rawDifficulty: string, rawResult: boolean) {
+  type Level = 'novice' | 'apprentice' | 'master' | 'avatar';
+  type Result = 'win' | 'loose';
+
+  const message: Record<Level, Record<Result, string>> = {
+    novice: {
+      win: 'Congratulations! Your bending skills are showing promise.',
+      loose: "Don't worry, even Aang fell a few times when he started.",
+    },
+    apprentice: {
+      win: "Well done! Your memory is as sharp as Sokka's wit.",
+      loose: 'Keep training! Even Zuko faced setbacks on his path to mastery.',
+    },
+    master: {
+      win: "Impressive! Your memory rivals Toph's earthbending precision.",
+      loose:
+        "It's okay, even Katara had moments of struggle before becoming a master.",
+    },
+    avatar: {
+      win: "You've mastered the elements of memory! You truly are the Avatar.",
+      loose:
+        'Even the Avatar stumbles sometimes. Keep practicing to reach your full potential.',
+    },
+  };
+
+  const difficulty = rawDifficulty.toLowerCase() as Level;
+  const result: Result = rawResult ? 'win' : 'loose';
+  return message[difficulty][result];
+}
